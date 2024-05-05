f16e65faaed4ec4e4aca250e1ffbb36490c9cd85
Hillary X <hillaryx@hillarys-air.mynetworksettings.com>
2024-05-05 14:57:29 -0400

    Add Weather Info

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 let weather = {
   "apiKey": "8a2c658f6e6f76f1a19d5fb0e81fcc7d",
-  fetchWeather: function () {
+  fetchWeather: function (city) {
     fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=Lagos&units=imperial&appId=8a2c658f6e6f76f1a19d5fb0e81fcc7d"
+      "https://api.openweathermap.org/data/2.5/weather?q=" 
+      + city 
+      + "&units=imperial&appId="
+      + this.apiKey
       )
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => this.displayWeather(data));
   },
-};
+  displayWeather: function(data) {
+    const { name }  = data; 
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name, icon, description, temp, humidity, speed);
+    document.querySelector(".city").innerText = "Weather in " + name;
+}
+}
